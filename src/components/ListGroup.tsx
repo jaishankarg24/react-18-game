10b27fa02a5e1f8a04ed8dcd6d96d7b579f670f1
jaishankar <jaishankarg24@gmail.com>
2023-04-22 12:32:47 +0530

    Different ways of conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,29 @@
 //import { Fragment } from "react";
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No items Found</p>
+  //     </>
+  //   );
+
+  //const message = items.length === 0 ? <p>No item Found</p> : null;
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item Found</p> : null;
+  };
   return (
     <>
       <h1>List</h1>
+
+      {/* {items.length === 0 ? <p>No item Found</p> : null} */}
+      {/* {message} */}
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item, key) => (
           <li key={key}>{item}</li>
