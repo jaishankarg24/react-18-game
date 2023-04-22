e763ef619aa61b262eceece4e27e159671e6d012
jaishankar <jaishankarg24@gmail.com>
2023-04-22 11:16:01 +0530

    creating components

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing for the Function
+function Message() {
+  // Jsx: JavaScript XML
+  const name = "";
+  //Js Expression
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
