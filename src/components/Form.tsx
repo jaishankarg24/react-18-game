a7ec382a885f819fb05c8b2af697fdb9b7e5222e
jaishankar <jaishankarg24@gmail.com>
2023-04-30 16:37:35 +0530

    Managing Forms with React Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,23 @@
 import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
+  //const form = useForm();
+  //console.log(form);
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log("Submitted");
-
-    console.log(person);
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
-
+  // <form onSubmit={handleSubmit((data) => console.log(data))}></form>
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
@@ -34,10 +28,7 @@ const Form = () => {
           Age
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
-          value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
@@ -51,6 +42,58 @@ const Form = () => {
 };
 
 export default Form;
+
+// const Form = () => {
+//   const [person, setPerson] = useState({
+//     name: "",
+//     age: 0,
+//   });
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     console.log("Submitted");
+
+//     console.log(person);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         <input
+//           onChange={(event) =>
+//             setPerson({ ...person, name: event.target.value })
+//           }
+//           value={person.name}
+//           id="name"
+//           type="text"
+//           className="form-control"
+//         />
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         <input
+//           onChange={(event) =>
+//             setPerson({ ...person, age: parseInt(event.target.value) })
+//           }
+//           value={person.age}
+//           id="age"
+//           type="number"
+//           className="form-control"
+//         />
+//       </div>
+//       <button type="submit" className="btn btn-primary">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default Form;
 
 // const Form = () => {
 //   const nameRef = useRef<HTMLInputElement>(null);
