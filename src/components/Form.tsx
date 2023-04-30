a1a6e06f78bb0f72821d9af4ba69730eb5112250
jaishankar <jaishankarg24@gmail.com>
2023-04-30 16:54:11 +0530

    Applying Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,19 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
   //const form = useForm();
   //console.log(form);
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -17,22 +26,31 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field must be atleast 3 characters.
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { pattern: /\d+/ })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.name?.type === "pattern" && <p>Please enter number for age.</p>}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
