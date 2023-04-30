236160e4902a7de922380165bf8b4afc5f473061
jaishankar <jaishankarg24@gmail.com>
2023-04-30 16:19:21 +0530

    Controlled Components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,14 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0,
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     console.log("Submitted");
-
-    if (nameRef.current != null) person.name = nameRef.current.value;
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
 
     console.log(person);
   };
@@ -21,13 +19,29 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
@@ -37,3 +51,41 @@ const Form = () => {
 };
 
 export default Form;
+
+// const Form = () => {
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = { name: "", age: 0 };
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     console.log("Submitted");
+
+//     if (nameRef.current != null) person.name = nameRef.current.value;
+//     if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
+
+//     console.log(person);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         <input ref={nameRef} id="name" type="text" className="form-control" />
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         <input ref={ageRef} id="age" type="number" className="form-control" />
+//       </div>
+//       <button type="submit" className="btn btn-primary">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default Form;
