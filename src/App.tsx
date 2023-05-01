b9cd41c99571cb98781ba7e0ff8b696183265c50
jaishankar <jaishankarg24@gmail.com>
2023-05-01 15:49:41 +0530

    Cancelling a Fetch Request

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ProductList from "./components/ProductList";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -188,20 +188,33 @@ function App() {
   //     .catch((err) => setError(err.message));
   // }, []);
 
-  useEffect(() => {
-    // get -> returns (await) promise - resolved ? response object : error object
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+  // useEffect(() => {
+  // get -> returns (await) promise - resolved ? response object : error object
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const res = await axios.get<User[]>(
+  //         "https://jsonplaceholder.typicode.com/users"
+  //       );
+  //       setUsers(res.data);
+  //     } catch (err) {
+  //       setError((err as AxiosError).message);
+  //     }
+  //   };
 
-    fetchUsers();
+  //   fetchUsers();
+  // }, []);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: abortController.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
   }, []);
 
   return (
